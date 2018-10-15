ecb2436e3c37f1df1bac97caca8891b344c1cabe
liubailin2017 <liubailinprivate@163.com>
2018-10-15 19:28:17 +0800

    修复时间显示问题

--- src/main/java/cap/mvc/model/wrapper/EventWrapper.java
+++ src/main/java/cap/mvc/model/wrapper/EventWrapper.java
@@ -3,12 +3,17 @@ package cap.mvc.model.wrapper;
 import cap.mvc.model.Event;
 import cap.util.FileName;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class EventWrapper extends Event{
 
     public String getFormatDt() {
-        return new Date(getDt()).toString();
+
+        SimpleDateFormat formator = new SimpleDateFormat("yyyy年mm月dd日");
+
+
+        return  formator.format(new Date(getDt()));
     }
 
     public String[] getDivPath() {
